fa4a165073718a53daa3dd7b5fea670fefc52002
Nuntuch <nuntuch13589@gmail.com>
2021-11-29 12:01:02 +0700

    EP2 :  rename file MytodoApplication.java to Application.java

--- mytodo/src/main/java/com/example/mytodo/Application.java
+++ mytodo/src/main/java/com/example/mytodo/Application.java
@@ -3,7 +3,7 @@ package com.example.mytodo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // add auto config ,scan manage component ,ect
 public class MytodoApplication {
 
 	public static void main(String[] args) {
