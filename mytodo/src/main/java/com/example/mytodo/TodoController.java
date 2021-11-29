96caf2b74421d5245ecec28ccd8e3f10173af8ec
Nuntuch <nuntuch13589@gmail.com>
2021-11-29 12:32:43 +0700

    EP3 : change return of getTodo function to list of Todo object

--- mytodo/src/main/java/com/example/mytodo/TodoController.java
+++ mytodo/src/main/java/com/example/mytodo/TodoController.java
@@ -3,16 +3,27 @@ package com.example.mytodo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController // ทำให้ class นี้ return เป็น json ได้เอง
 public class TodoController {
+
+    private List<Todo> todos = new ArrayList<>();
+
+
+
     public TodoController(){
+        todos.add(new Todo(1,"iBlur"));
+        todos.add(new Todo(2,"Cat"));
+        todos.add(new Todo(3,"Dog"));
 
     }
     //   .../todo
     // GET
     @GetMapping("/todo")
-    public String getTodos(){
-        return  "iBlurBlur";
+    public List<Todo> getTodos(){
+        return  todos;
     }
 
 }
