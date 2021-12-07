89d927fa9c2de02fffdaa0446f71261d3ea66798
Nuntuch <nuntuch13589@gmail.com>
2021-12-07 15:29:17 +0700

    Revert "Section8: make the sign-up web service endpoint public"
    This reverts commit 0663bf7b7cb61dfb414744610bfbcc748d5d19ec.

--- mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/service/UserService.java
@@ -1,9 +1,7 @@
 package com.appsdeveloperblog.app.ws.service;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
-
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 
-public interface UserService extends UserDetailsService{
+public interface UserService {
 	UserDto createUser(UserDto user);
 }
