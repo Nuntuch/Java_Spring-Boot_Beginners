0c0d4a63796e9dedc3282eb893a0b8e8077db9e8
Nuntuch <nuntuch13589@gmail.com>
2021-12-09 13:40:22 +0700

    Section9: change to reading token secret from a property file

--- mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/SecurityConstants.java
@@ -1,12 +1,22 @@
 package com.appsdeveloperblog.app.ws.security;
 
+import org.springframework.boot.SpringApplication;
+
+import com.appsdeveloperblog.app.ws.SpringApplicationContext;
+
 public class SecurityConstants {
 	
 	public static final long EXPIRATION_TIME = 864000000; //10 days
-	public static final String TOKEN_PREFIX = "Bearer";
+	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final String SIGN_UP_URL = "/users";
-	public static final String TOKEN_SECRET = "jf9i4jgu83nflojfu57ejf7";
+//	public static final String TOKEN_SECRET = "jf9i4jgu83nflojfu57ejf7";
+	
+	public static String getTokenSecret() {
+		AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+		System.out.println("[DEBUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG] appProperties.getTokenSecret() : " + appProperties.getTokenSecret());
+		return appProperties.getTokenSecret();
+	}
 	
 	
 		
