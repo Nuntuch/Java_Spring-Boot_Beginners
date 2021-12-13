30bb0522a41b3ba32be701a4a3801b37890862a6
Unknown <nuntuch13589@gmail.com>
2021-12-13 14:04:00 +0700

    Section12: return custom error object representation

--- mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionsHandler.java
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.app.ws.exceptions;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +9,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.ErrorMessage;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 	
 	@ExceptionHandler(value = {UserServiceException.class})
 	public ResponseEntity<Object> handleUserServiceException (UserServiceException ex, WebRequest request){
 //		return new ResponseEntity<>(ex.getMessage(),new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
-		return new ResponseEntity<>(ex,new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		
+		return new ResponseEntity<>(errorMessage,new HttpHeaders(),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 }
