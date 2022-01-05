85fb11de9d545d23a18ef82cc8033a0d9f7f1f57
Nuntuch <nuntuch13589@gmail.com>
2022-01-05 16:30:15 +0700

    Section29: create native SQL query use JPQL to select specific fields only

--- mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/test/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepositoryTest.java
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/test/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepositoryTest.java
@@ -148,6 +148,25 @@ class UserRepositoryTest {
 	}
 	
 	
+	@Test
+	final void testGetUserEntityFullNameById()
+	{
+		String userId = "1a2b3c";
+		List<Object[]> records =  userRepository.getUserEntityFullNameById(userId);
+		
+        assertNotNull(records);
+        assertTrue(records.size() == 1);
+        
+        Object[] userDetails = records.get(0);
+      
+        String firstName = String.valueOf(userDetails[0]);
+        String lastName = String.valueOf(userDetails[1]);
+
+        assertNotNull(firstName);
+        assertNotNull(lastName);
+	}
+	
+	
 	private void createRecrods()
 	{
 		// Prepare User Entity
