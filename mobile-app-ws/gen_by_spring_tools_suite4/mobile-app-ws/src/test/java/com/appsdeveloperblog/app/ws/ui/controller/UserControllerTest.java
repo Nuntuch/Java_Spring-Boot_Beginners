a117826b6c090da7f5a4f2df5480fde5d0135d35
Nuntuch <nuntuch13589@gmail.com>
2022-01-06 14:34:07 +0700

    Section24: create UserController test

---
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/test/java/com/appsdeveloperblog/app/ws/ui/controller/UserControllerTest.java
@@ -0,0 +1,19 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	final void testGetUser() {
+		fail("Not yet implemented");
+	}
+
+}
