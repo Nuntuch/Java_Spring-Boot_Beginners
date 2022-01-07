1652a509130dbc0d4b927131d55917e9e4dc0d05
Nuntuch <nuntuch13589@gmail.com>
2022-01-07 14:30:32 +0700

    Section32: enable Swagger URLs and view json documentation
    http://localhost:8046/mobile-app-ws/swagger-ui.html

--- mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/SwaggerConfig.java
@@ -7,7 +7,12 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 
+import javassist.Loader.Simple;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -21,30 +26,30 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-    Contact contact = new Contact(
-            "Sergey Kargopolov",
-            "http://www.appsdeveloperblog.com", 
-            "developer@appsdeveloperblog.com"
-    );
-    
-    List<VendorExtension> vendorExtensions = new ArrayList<>();
-	
-	ApiInfo apiInfo = new ApiInfo(
-			"Photo app RESTful Web Service documentation",
-			"This pages documents Photo app RESTful Web Service endpoints", 
-			"1.0",
-			"http://www.appsdeveloperblog.com/service.html", 
-			contact, 
-			"Apache 2.0",
-			"http://www.apache.org/licenses/LICENSE-2.0", 
-			vendorExtensions);
+//    Contact contact = new Contact(
+//            "Sergey Kargopolov",
+//            "http://www.appsdeveloperblog.com", 
+//            "developer@appsdeveloperblog.com"
+//    );
+//    
+//    List<VendorExtension> vendorExtensions = new ArrayList<>();
+//	
+//	ApiInfo apiInfo = new ApiInfo(
+//			"Photo app RESTful Web Service documentation",
+//			"This pages documents Photo app RESTful Web Service endpoints", 
+//			"1.0",
+//			"http://www.appsdeveloperblog.com/service.html", 
+//			contact, 
+//			"Apache 2.0",
+//			"http://www.apache.org/licenses/LICENSE-2.0", 
+//			vendorExtensions);
 
 	@Bean
 	public Docket apiDocket() {
 
 		Docket docket = new Docket(DocumentationType.SWAGGER_2)
-				.protocols(new HashSet<>(Arrays.asList("HTTP","HTTPs")))
-				.apiInfo(apiInfo)
+//				.protocols(new HashSet<>(Arrays.asList("HTTP","HTTPs")))
+//				.apiInfo(apiInfo)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.appsdeveloperblog.app.ws")).paths(PathSelectors.any())
 				.build();
@@ -52,5 +57,19 @@ public class SwaggerConfig {
 		return docket;
 
 	}
+	
+	@Bean
+	public LinkDiscoverers discovers() {
+		
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+		
+	}
+	
+	
+	
+	
 
 }
