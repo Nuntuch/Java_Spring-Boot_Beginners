af46a4625e0e06e68a232019b0f8c36e93ba13e6
Nuntuch <nuntuch13589@gmail.com>
2022-01-10 14:51:16 +0700

    Section33: add user roles @ManyToMany mapping for user entity

--- mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.apache.log4j.Logger;
+
+
+import java.util.Collection;
+
+import javax.persistence.JoinColumn;
+
 
 //@Entity(name= "users")
 
@@ -57,7 +66,12 @@ public class UserEntity implements Serializable{
 	private List<AddressEntity> addresses;
 	
 
-
+	@ManyToMany(cascade= { CascadeType.PERSIST }, fetch = FetchType.EAGER )
+	@JoinTable(name="users_roles", 
+			joinColumns=@JoinColumn(name="users_id",referencedColumnName="id"), 
+			inverseJoinColumns=@JoinColumn(name="roles_id",referencedColumnName="id"))
+	private Collection<RoleEntity> roles;
+	
 
 	public long getId() {
 		return id;
