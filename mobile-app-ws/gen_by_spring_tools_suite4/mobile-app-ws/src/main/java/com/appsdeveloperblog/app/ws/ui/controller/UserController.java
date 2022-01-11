452aaf27c35de0ef378ae3553a85bbc606eb3a86
Nuntuch <nuntuch13589@gmail.com>
2022-01-11 23:59:12 +0700

    Section33: add to use @PostAuthorize Annotation

--- mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/gen_by_spring_tools_suite4/mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -70,6 +71,7 @@ public class UserController {
 	@Autowired
 	AddressService addressesService;
 	
+	@PostAuthorize("hasRole('ADMIN') or returnObject.userId == principal.userId")
 	@ApiOperation(value="The Get User Details Web Service Endpoint",
 			notes="${userController.GetUser.ApiOperation.Notes}")
 	@GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
